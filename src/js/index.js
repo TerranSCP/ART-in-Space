4cd70272c2681095af52255888ff7c496524bfb5
TerranSCP <baatezy@yandex.ru>
2018-05-15 12:47:49 +0400

    added cells controls, animation fixed, running line after game end

--- src/js/index.js
+++ src/js/index.js
@@ -79,7 +79,8 @@ images.clearLinks();
 //view
 
 class controlButtonsRender {
-    constructor(element,backgrounds) {
+    constructor(element, backgrounds) {
+
         this.element = element;
         this.backgrounds = [
             'img/background/space0.jpg',
@@ -97,9 +98,9 @@ class controlButtonsRender {
 
     render() {
 
-         const bgLink = this.backgrounds[Math.floor(Math.random() * (9 - 0 + 1)) + 0];
-         document.body.style.cssText = `background-image:url(${bgLink})`;
-         
+        const bgLink = this.backgrounds[Math.floor(Math.random() * (9 - 0 + 1)) + 0];
+        document.body.style.cssText = `background-image:url(${bgLink})`;
+
 
         this.element.insertAdjacentHTML('afterbegin',
             '<h1 class="header--text">Find a pair!</h1>\
@@ -107,12 +108,35 @@ class controlButtonsRender {
      <br>\
      <button class="control--button  button__4">4x4</button> <button class="control--button  button__5">5x5</button> <button class="control--button  button__6">6x6</button>'
         );
+
+        this._score();
+    }
+
+    _score() {
+        let score = new scoreRender()._render();
     }
 
 }
 
+
+class scoreRender {
+
+    constructor() {
+
+    }
+
+    _render() {
+        const control = document.querySelector('.control--container');
+        control.insertAdjacentHTML('beforeend', '<div class = "score">Turns count: <span class = "turns">0</span></div>')
+    }
+
+}
+
+
 let buttons = new controlButtonsRender(document.querySelector('.control--container'));
 buttons.render();
+
+
 
 //запуск вот этого рендера производит контроллер (строит таблицу)
 
@@ -130,7 +154,7 @@ class tableRender {
 
         this.elem = elem;
         let count = parseInt(elem.innerHTML);
-      
+
 
         let table = document.createElement('table');
 
@@ -138,7 +162,7 @@ class tableRender {
             let row = document.createElement('tr');
             for (let c = count; c > 0; c--) {
                 let cell = document.createElement('td');
-                cell.classList.add('cell');            
+                cell.classList.add('cell');
                 row.appendChild(cell);
             }
             table.appendChild(row);
@@ -151,52 +175,89 @@ class tableRender {
 
     _imageRender() {
 
-          let cells = document.querySelectorAll('.cell');
-          const random =  this._randomizer(cells);
-         
-         for( let i = 0; i<cells.length;i++){
+        let cells = document.querySelectorAll('.cell');
+        const random = this._randomizer(cells);
 
-            if(cells.length === 25 && i === 12){
-                cells[12].style.visibility = 'hidden';    
+        for (let i = 0; i < cells.length; i++) {
+
+            if (cells.length === 25 && i === 12) {
+                cells[12].style.visibility = 'hidden';
                 continue;
             }
             let glink = sessionStorage.getItem(`link-${random[i]}`);
-            cells[i].insertAdjacentHTML('afterbegin',`<img class="image"  src=${glink}></img> <div class = "choke"></div>`);
-            
-         }
+            cells[i].insertAdjacentHTML('afterbegin', `<img class="image"  src=${glink}></img> <div class = "choke"></div>`);
 
-         cells[24].firstElementChild.src = sessionStorage.getItem(`link-${random[12]}`);
-         this._initializeCells();
-        
+        }
+
+        if (cells.length === 25) {
+            cells[24].firstElementChild.src = sessionStorage.getItem(`link-${random[12]}`);
+        }
+
+
+        this._initializeCells();
+
     }
 
 
-    _randomizer(cells){
-    
+    _randomizer(cells) {
+
         this.cells = cells;
 
         const arr = [];
-        for(let c = 0; c<cells.length/2;c++ ){
-           arr.push(c);
-           arr.push(c);
+        for (let c = 0; c < cells.length / 2; c++) {
+            arr.push(c);
+            arr.push(c);
         }
 
-        if(cells.length == 25){
+        if (cells.length == 25) {
             arr.pop();
             arr.pop();
         }
 
-        return arr.sort(()=>{
-               return 0.5-Math.random();
+        return arr.sort(() => {
+            return 0.5 - Math.random();
         });
-        
+
     }
 
-    _initializeCells(){
-        const init = new cellController._event();
+    _initializeCells() {
+        const init = new cellController()._event();
     }
 
 }
+
+class runningLineRender {
+    constructor() {
+
+    }
+
+    _render() {
+        const parent = document.body;
+        const line = document.createElement('div');
+        line.innerHTML = 'SUCCESS';
+        line.id = 'running--line';
+        line.style.left = '0%'
+        parent.appendChild(line);
+
+        const interval = 50;
+        let timePassed = 0
+
+        const start = setInterval(() => {
+
+            timePassed += interval;
+
+            if (timePassed > 2299) {
+                const stop = clearInterval(start);
+                const remove = new runningLineController()._remove(line);
+            }
+
+            line.style.left = 2 + parseInt(line.style.left) + "%";
+
+        }, interval);
+    }
+}
+
+
 
 
 //controller
@@ -213,7 +274,7 @@ class ButtonController {
         this.click = event;
         if (event.target.tagName == "BUTTON") {
             let tablerun = new tableRender(document.querySelector('.game--container')).render(event.target); // запускает рендер таблицы
-            
+
         }
 
     }
@@ -224,15 +285,116 @@ let bc = new ButtonController(document.querySelector('.control--container'));
 
 
 class cellController {
-    
-    constructor(){
+
+    constructor() {
 
     }
-  
-    _event(){
-       const table = document.querySelector()
+
+    _event() {
+
+        let counter = 0
+        const table = document.querySelector('table');
+
+        table.addEventListener('click', (event) => {
+
+            counter++;
+            document.querySelector('.turns').innerHTML = counter;
+
+
+
+            if (event.target.tagName === 'DIV') {
+
+                event.target.parentElement.classList.add('open');
+
+                setTimeout(() => {
+                    event.target.style.display = "none";
+                }, 500);
+
+                let opened = document.querySelectorAll('.open');
+
+                if (opened.length > 1 && opened[0].firstElementChild.src !== opened[1].firstElementChild.src) {
+
+                    setTimeout(() => {
+
+                        for (let i = 0; i < opened.length; i++) {
+
+                            try {
+                                opened[i].classList.remove('open');
+                                opened[i].lastChild.style.display = "block";
+                            }
+                            catch (e) {
+
+                            }
+                        }
+
+                    }, 550)
+
+                }
+
+                if (opened.length > 1 && opened[0].firstElementChild.src === opened[1].firstElementChild.src) {
+
+                    setTimeout(() => {
+
+                        for (let i = 0; i < opened.length; i++) {
+                            opened[i].firstElementChild.classList.add('opened');
+
+                        }
+
+                    }, 550);
+
+
+                    setTimeout(() => {
+
+                        for (let i = 0; i < opened.length; i++) {
+                            opened[i].classList.remove('open');
+                            opened[i].removeChild(opened[i].lastElementChild);
+                            opened[i].removeChild(opened[i].firstElementChild);
+                            opened[i].style.visibility = 'hidden';
+                        }
+
+                        this._destructor(table);
+
+                    }, 850);
+
+
+
+                }
+
+            }
+
+        });
+
+
     }
 
+    _destructor(table) {
+
+        this.table = table;
+
+        if (document.querySelectorAll('.image').length == 0) {
+            table.remove();
+            const rl = new runningLineController().start();
+        }
+
+    }
+
+
+}
+
+class runningLineController {
+
+    constructor() {
+
+    }
+
+    start() {
+        const start = new runningLineRender()._render();
+    }
+
+    _remove(line) {
+        this.line = line;
+        line.remove();
+    }
 
 }
 
